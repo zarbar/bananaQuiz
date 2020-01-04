c3fa4595651e5a5bf9e941db0441dddf9f481412
Zara <48896844+zarbar@users.noreply.github.com>
2020-01-04 13:07:16 +0000

    css re-done to be mobile responsive on questions.js

--- src/components/questions/questionlist.js
+++ src/components/questions/questionlist.js
@@ -3,50 +3,78 @@ export default function questionList() {
         {
             question_num: 'Question One',
             question_title: 'Pick a Colour:',
-            answers: ['White', 'Brown', 'Yellow', 'Black'],
-            answer_key: ['split', 'bread', 'regular', 'peel']
+            options: [{ 'option': 'White', 'answer': 'split' },
+            { 'option': 'Brown', 'answer': 'bread' },
+            { 'option': 'Yellow', 'answer': 'regular' },
+            { 'option': 'Black', 'answer': 'peel' },
+            ]
         },
         {
             question_num: 'Question Two',
             question_title: 'Pick a Consistency:',
-            answers: ['Mush', 'Foam', 'Cream', 'Banana'],
-            answer_key: ['peel', 'bread', 'split', 'regular']
+            options: [
+                { 'option': 'Mush', 'answer': 'peel' },
+                { 'option': 'Foam', 'answer': 'bread' },
+                { 'option': 'Cream', 'answer': 'split' },
+                { 'option': 'Banana', 'answer': 'regular' },]
         },
         {
             question_num: 'Question Three',
             question_title: 'Pick a Smell:',
-            answers: ['Fresh out the oven', 'Sugar', 'Floor wax', 'Banana'],
-            answer_key: ['bread', 'split', 'peel', 'regular']
+            options: [
+                { 'option': 'Fresh out the oven', 'answer': 'bread' },
+                { 'option': 'Sugar', 'answer': 'split' },
+                { 'option': 'Floor wax', 'answer': 'peel' },
+                { 'option': 'Banana', 'answer': 'regular' },]
         },
         {
             question_num: 'Question Four',
             question_title: 'Pick a Taste:',
-            answers: ['Sweet', 'Savoury', 'Bitter', 'Banana'],
-            answer_key: ['split', 'bread', 'peel', 'regular']
+            options: [
+                { 'option': 'Sweet', 'answer': 'split' },
+                { 'option': 'Savoury', 'answer': 'bread' },
+                { 'option': 'Bitter', 'answer': 'peel' },
+                { 'option': 'Banana', 'answer': 'regular' },]
         },
         {
             question_num: 'Question Five',
             question_title: 'Pick an Interaction:',
-            answers: ['Slipping over', 'Cutting a slice', 'Scooping a spoonful', 'Peeling a banana'],
-            answer_key: ['peel', 'bread', 'split', 'regular']
+            options: [
+                { 'option': 'Slipping over', 'answer': 'peel' },
+                { 'option': 'Cutting a slice', 'answer': 'bread' },
+                { 'option': 'Scooping a spoonful', 'answer': 'split' },
+                { 'option': 'Peeling a banana', 'answer': 'regular' },
+            ]
         },
         {
             question_num: 'Question Six',
             question_title: 'Pick an Effect:',
-            answers: ['Brain freeze', 'being full', 'Increased potassium', 'Body pain'],
-            answer_key: ['split', 'bread', 'regular', 'peel']
+            options: [
+                { 'option': 'Brain freeze', 'answer': 'split' },
+                { 'option': 'being full', 'answer': 'bread' },
+                { 'option': 'Increased potassium', 'answer': 'regular' },
+                { 'option': 'Body pain', 'answer': 'peel' },
+            ]
         },
         {
             question_num: 'Question Seven',
             question_title: 'Pick an Occasion:',
-            answers: ['Birthday party', 'Brunch', "Not looking where you're going", 'Eating a banana'],
-            answer_key: ['split', 'bread', 'peel', 'regular']
+            options: [
+                { 'option': 'Birthday party', 'answer': 'split' },
+                { 'option': 'Brunch', 'answer': 'bread' },
+                { 'option': "Not looking where you're going", 'answer': 'peel' },
+                { 'option': 'Eating a banana', 'answer': 'regular' },
+            ]
         },
         {
             question_num: 'Question Eight',
             question_title: 'Pick a Response:',
-            answers: ['That was a good banana', 'Yummy', "Ooh, smells good!", 'Why me?'],
-            answer_key: ['regular', 'split', 'bread', 'peel']
+            options: [
+                { 'option': 'That was a good banana', 'answer': 'regular' },
+                { 'option': 'Yummy', 'answer': 'split' },
+                { 'option': "Ooh, smells good!", 'answer': 'bread' },
+                { 'option': 'Why me?', 'answer': 'peel' },
+            ]
         }
     ])
 }
--- src/components/questions/questions.js
+++ src/components/questions/questions.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Container, Card } from 'react-bootstrap';
 import './questions.css';
 import Sure from '../results/sure';
 import Result from '../results/final_result';
@@ -9,59 +8,13 @@ import questionList from './questionlist';
 export default function Questions() {
 
     const [current_question, setCurrent_question] = useState(questionList()[0]);
-    const [user_answer, setUser_answer] = useState([]);
+    const [user_answers, setUser_answers] = useState([]);
     const [show_sure_page, setSure] = useState(false);
     const [show_final_result, setResults] = useState(false);
 
-
-    //increase nextQuestion number if under 7, if over 7 activate show_sure_page
-    // let nextQuestion = (answer) => {
-    //     const { current_question } = this.state;
-    //     if (current_question === 7) {
-    //         this.setState({
-    //             show_sure_page: true
-    //         });
-    //     } else {
-    //         this.setState({ current_question: current_question + 1 });
-    //     };
-    // };
-
-
-    //pushes answer type to user_answers array
-    // let pushAnswer = (answerKey) => {
-    //         this.setState({
-    //         user_answers: [...this.state.user_answers, answerKey]
-    //     });
-    // };
-
-    // let workoutFinalResult = () => {
-    //     const { modeAnswer } = this.state;
-    //     const { modeArray } = this.state;
-    //     this.setState({ modeAnswer: modeArray[0] });
-    //     console.log(modeAnswer)
-    // }
-
-    // let goToResult = () => {
-    //     this.setState({ show_sure_page: false, show_final_result: true })
-    // }
-
-    // let workoutResult = (ary) => {
-    //     var counter = {};
-    //     var mode = [];
-    //     var max = 0;
-    //     for (var i in ary) {
-    //         if (!(ary[i] in counter))
-    //             counter[ary[i]] = 0;
-    //         counter[ary[i]]++;
-
-    //         if (counter[ary[i]] === max)
-    //             mode.push(ary[i]);
-    //         else if (counter[ary[i]] > max) {
-    //             max = counter[ary[i]];
-    //             mode = [ary[i]];
-    //         }
-    //     }
-    // }
+    useEffect(() => {
+        console.log(user_answers);
+    })
 
     // if (this.state.show_sure_page) {
     //     return <Sure goToResult={goToResult} />;
@@ -70,73 +23,38 @@ export default function Questions() {
     //     return <Result workoutFinalResult={workoutFinalResult} />;
     // }
 
+
     return (
 
         //Background image
-        <div className="backBlue" >
+        <main className="backBlue" >
 
             {/* Yellow box */}
-            < Container >
-                <Card className="text-center cardBox" border="light">
-                    <Card.Body style={{
-                        backgroundColor: '#FAE700', color: 'black', fontFamily: 'Ribeye Marrow',
-                        fontStyle: 'normal', fontSize: '60px', lineHeight: '74px'
-                    }}>
+            < div className='yellowBox'>
 
-                        {/* Question number */}
-                        <Container>
-                            <span className="underline">{current_question.question_num}</span>
-                        </Container>
-                        {/* 
-                            Question */}
-                        <Card.Text><span className="questionFont">{current_question.question_title}</span>
-                        </Card.Text>
+                {/* Question number */}
+                <h2>{current_question.question_num}</h2>
 
-                        {/* option 1 */}
-                        <Container>
-                            <Button variant="outline" className="butOff" onClick={() => {
-                                pushAnswer(current_question.answer_key[0]);
-                                nextQuestion(current_question.answers[0]);
-                                workoutResult(this.state.user_answers);
-                                workoutFinalResult();
-                            }}>
-                                {current_question.answers[0]}
+                {/* Question */}
+                <h3 className="questionFont">{current_question.question_title}</h3>
 
-                            </Button>
-                        </Container>
-
-                        {/* option 2 */}
-                        <Container>
-                            <Button variant="outline-*" className="butOff" onClick={() => {
-                                pushAnswer(current_question.answer_key[1]);
-                                nextQuestion(current_question.answers[1]);
-                                workoutResult(this.state.user_answers);
-                                workoutFinalResult();
-                            }} >{current_question.answers[1]}</Button>
-                        </Container>
-
-                        {/* option 3 */}
-                        <Container>
-                            <Button variant="outline-*" className="butOff" onClick={() => {
-                                pushAnswer(current_question.answer_key[2]);
-                                nextQuestion(current_question.answers[2]);
-                                workoutResult(this.state.user_answers)
-                            }}>{current_question.answers[2]}</Button>
-                        </Container>
-
-                        {/* option 4 */}
-                        <Container>
-                            <Button variant="outline-*" className="butOff" onClick={() => {
-                                pushAnswer(current_question.answer_key[3]);
-                                nextQuestion(current_question.answers[3]);
-                                workoutResult(this.state.user_answers)
-                            }}>{current_question.answers[3]}</Button>
-                        </Container>
-
-
-                    </Card.Body>
-                </Card >
-            </Container >
-        </div>
+                {/* options */}
+                {current_question.options.map((optionItem, index) => {
+                    const answer = optionItem.answer;
+                    return (
+                        <div key={index}>
+                            <button
+                                onClick={() => {
+                                    let currentAnswers = user_answers;
+                                    setUser_answers([...currentAnswers, answer]);
+                                }}
+                                className='buttonPadding'>
+                                <h1>{optionItem.option}</h1>
+                            </button>
+                        </div>
+                    )
+                })}
+            </div >
+        </main>
     )
 }
