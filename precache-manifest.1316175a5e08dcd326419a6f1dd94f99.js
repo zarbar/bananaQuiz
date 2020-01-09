e84c2934bef91c89df25ef9320c6b039fa374966
Zara <48896844+zarbar@users.noreply.github.com>
2020-01-09 15:15:36 +0000

    Updates

---
+++ precache-manifest.1316175a5e08dcd326419a6f1dd94f99.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3712ce40d0d134ef2567e7a321c1f8",
+    "url": "/bananaQuiz/index.html"
+  },
+  {
+    "revision": "e7b3b349dda16c31f265",
+    "url": "/bananaQuiz/static/css/main.d622235b.chunk.css"
+  },
+  {
+    "revision": "16d4cc488d2a5a0df1b5",
+    "url": "/bananaQuiz/static/js/2.60ad2b16.chunk.js"
+  },
+  {
+    "revision": "5e8db4a88494a2bc0319f78b7aa3661f",
+    "url": "/bananaQuiz/static/js/2.60ad2b16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7b3b349dda16c31f265",
+    "url": "/bananaQuiz/static/js/main.de9739fd.chunk.js"
+  },
+  {
+    "revision": "e6cc3e03a4361793c33c",
+    "url": "/bananaQuiz/static/js/runtime-main.3db80f41.js"
+  },
+  {
+    "revision": "3d9a6a39cf38dd352de6ef80c6834f5d",
+    "url": "/bananaQuiz/static/media/banana.3d9a6a39.jpg"
+  },
+  {
+    "revision": "9bac3c7c1b7e827c3450ee2b7c8040cb",
+    "url": "/bananaQuiz/static/media/bananabread.9bac3c7c.jpg"
+  },
+  {
+    "revision": "24d7900138b18ba8a91d57e679c9714d",
+    "url": "/bananaQuiz/static/media/bananapeel.24d79001.jpg"
+  },
+  {
+    "revision": "575292f9e9031caccd88397270e38ba8",
+    "url": "/bananaQuiz/static/media/bananasplit.575292f9.jpg"
+  },
+  {
+    "revision": "b631c215f59d1f1d64b273fc5fd8d197",
+    "url": "/bananaQuiz/static/media/bananatile.b631c215.jpg"
+  },
+  {
+    "revision": "f8c3a03dcbae550806c25bb1723404df",
+    "url": "/bananaQuiz/static/media/newBananaBackground.f8c3a03d.jpg"
+  }
+]);
